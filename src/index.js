8c08ef0a78d99cc29b323a79ab8005dbdd1530a7
Cristian Perez <cperezlecaros@gmail.com>
2026-04-09 17:51:10 -0400

    feat: Implement database schemas and configure drizzle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import express from "express";
+const app = express();
+const PORT = 8000;
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Sport Master API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
